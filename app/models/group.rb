f47394c0e93b2463cc987ebb81a7558b6786859b
Amy Hogue <hogue.a@gmail.com>
2018-03-28 11:28:38 -0600

    Create 'GroupsUsers' join table for many:many association

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,4 +9,5 @@
 #
 
 class Group < ApplicationRecord
+  has_and_belongs_to_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328165428) do
+ActiveRecord::Schema.define(version: 20180328171416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180328165428) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.bigint "group_id"
+    t.bigint "user_id"
+    t.index ["group_id"], name: "index_groups_users_on_group_id"
+    t.index ["user_id"], name: "index_groups_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +45,6 @@ ActiveRecord::Schema.define(version: 20180328165428) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups_users", "groups"
+  add_foreign_key "groups_users", "users"
 end
