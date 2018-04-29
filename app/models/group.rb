cb7156b16515fc0d5b2868d3b8797796fb191890
Amy Hogue <hogue.a@gmail.com>
2018-04-29 13:11:19 -0600

    Add dependent destroy to group's meals

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,6 +11,6 @@
 class Group < ApplicationRecord
   has_many :groups_users, dependent: :destroy
   has_many :users, through: :groups_users
-  has_many :meals
+  has_many :meals, dependent: :destroy
   accepts_nested_attributes_for :groups_users, reject_if: :all_blank, allow_destroy: true
 end
