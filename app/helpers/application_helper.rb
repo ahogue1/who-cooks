f41e217fb9c9a773d72eb61fce40029f5871a761
Amy Hogue <hogue.a@gmail.com>
2018-05-17 21:16:16 -0600

    Pick between uploaded phot and facebook photo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
   def avatar_url(user)
-    user.photo || "http://placehold.it/30x30"
+    return "#{user.facebook_photo}&height=50&width=50" if user.facebook_photo
+    user.photo_url(:thumb) || "http://placehold.it/30x30"
   end
 
 end
